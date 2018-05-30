6200a38927aa6bbac821c5223eaf0888a7b76bd5
tonics96 <33870890+tonics96@users.noreply.github.com>
2018-05-30 15:48:25 +0200

    Delete runMe.sh

--- MapReduce/runMe.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-gnome-terminal -e "python registry.py"
-gnome-terminal -e "python host1.py"
-gnome-terminal -e "python host2.py"
-gnome-terminal -e "python host3.py"
-gnome-terminal -e "python reducer.py"
-gnome-terminal -e "python server.py"
